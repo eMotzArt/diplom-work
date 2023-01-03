6d443c20766e2190d1069d08c00c1e965020eb1a
emotzart <johnshuabratsk@gmail.com>
2023-01-03 22:35:15 +0700

    Dajngo Admin configured

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import User
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser',)
+    readonly_fields = ('last_login', 'date_joined',)
+    exclude = ('password',)
+
+admin.site.register(User, CustomUserAdmin)
