97a651b963e827d4b5401c5c53d4bc76e3baedda
emotzart <johnshuabratsk@gmail.com>
2023-02-08 19:52:21 +0700

    Added docstings

--- bot/manager.py
+++ bot/manager.py
@@ -1,14 +1,15 @@
 import datetime
-import random
-import string
 
 from bot.models import TgUser, TgState
 from bot.tg.client import TgClient
-from bot.tg.dc import GetUpdatesResponse
+from bot.tg.dc import GetUpdatesResponse, UpdateObj
 from goals.models import Goal, Category
 
 
 class BotManager:
+    """Класс менеджера-бота, получающего и обрабатывающего новую информацию от бота и отправляющего ответы
+    
+    """
     user_id: int
     chat_id: int
     message: str
@@ -23,38 +24,63 @@ class BotManager:
         self.offset = 0
 
     def get_updates(self):
+        """Метод для получения и сохранения в экземпляре всех новых сообщений с telegram-клиента
+
+        """
         self.response = self.tg_client.get_updates(offset=self.offset)
 
-    def get_offset(self):
+    def get_offset(self) -> int:
+        """Метод возвращающий текущий оффсет для последующего запроса на получение новых сообщений
+
+        """
         return self.offset
 
-    def user_exists(self):
+    def user_exists(self) -> bool:
+        """Метод проверяющий наличие в базе текущего пользователя по его id и id чата
+
+        """
         return TgUser.objects.filter(tg_user_id=self.user_id, tg_chat_id=self.chat_id).exists()
 
-    def get_user(self):
+    def get_user(self) -> TgUser:
+        """Метод возвращающий (или создающий, в случае отсутствия) пользователя
+
+        """
         user, _ = TgUser.objects.get_or_create(tg_user_id=self.user_id, tg_chat_id=self.chat_id)
         return user
 
-    def _get_random_code(self, length):
-        letters = string.ascii_lowercase
-        code = ''.join(random.choice(letters) for i in range(length))
-        return code
-
     def verificate_user(self):
+        """Метод записывает в базу сгенерированный случайный код для верификации и отправляет его пользователю
+
+        """
         verification_code = self.current_user.get_verification_code()
         self.tg_client.send_message(self.chat_id, f"Your verification code is: [{verification_code}]\n"
                                         f"Please put the verification code in to web-site")
-    def is_app_user(self):
+
+    def is_app_user(self) -> bool:
+        """Метод проверяет привязан ли пользователь приложения к текущему телеграм-пользователю (идентифицирован ли он)
+
+        """
         return bool(self.current_user.app_user)
 
     def get_user_state(self):
+        """Метод созхраняет в экземпляр найденную или создает для пользователя TgState запись в БД
+        (для отслеживания на каком этапе создания новой цели пользователь находится)
+
+        """
         state, _ = TgState.objects.get_or_create(tg_user=self.current_user)
-        self.current_state = state
+        self.current_state: TgState = state
 
     def get_user_step(self):
-        self.current_step = self.current_state.step
+        """Метод возвращает текущий "этап" пользователя
+        (0 - не создает, 1-2-3 - задает категорию-задает название цели-этап генерации новой цели)
 
-    def get_user_goals(self):
+        """
+        self.current_step: int = self.current_state.step
+
+    def get_user_goals(self) -> str:
+        """Метод формирует и возвращает строку со списком целей пользователя или строку с ответом что их нет
+
+        """
         accessed_categories = Category.objects.filter(board__participants__user=self.current_user.app_user,
                                                       is_deleted=False)
         user_goals = Goal.objects.filter(category__in=accessed_categories, status__in=[1, 2, 3]).all()
@@ -64,18 +90,27 @@ class BotManager:
             return "\n".join(goals_list)
         return "You have no goals"
 
-    def get_user_categories(self):
+    def get_user_categories(self) -> str:
+        """Метод формирует и возвращает строку со списком категорий пользователя или строку с ответом что их нет
+
+        """
         user_categories = Category.objects.filter(board__participants__user=self.current_user.app_user, is_deleted=False)
 
         if user_categories:
             categories_list = [f"#{cat.id} {cat.title}" for cat in user_categories]
             return "\n".join(categories_list)
 
-    def set_user_step(self, step):
+    def set_user_step(self, step: int):
+        """Метод записывает в state пользователя его текущий шаг, а так же в экземпляр, для удобства доступа
+
+        """
         self.current_state.set_step(step)
         self.current_step = step
 
-    def get_category_by_num_or_title(self):
+    def get_category_by_num_or_title(self) -> Category:
+        """Метод находит и возвращает категорию пользователя по номеру или названию
+
+        """
         filters = {
             'board__participants__user': self.current_user.app_user,
             'is_deleted': False,
@@ -90,10 +125,16 @@ class BotManager:
         user_categories = Category.objects.filter(**filters).first()
         return user_categories
 
-    def set_user_category(self, category):
+    def set_user_category(self, category: Category):
+        """Метод записывает в state (состояние) пользователя категорию, к которой он хочет создать цель
+
+        """
         TgState.objects.filter(tg_user=self.current_user).update(category=category)
 
-    def create_goal(self):
+    def create_goal(self) -> Goal:
+        """Метод создает и возвращает цель
+
+        """
         created_goal = Goal.objects.create(user=self.current_user.app_user,
                                            title=self.current_state.title,
                                            category=self.current_state.category,
@@ -101,6 +142,10 @@ class BotManager:
         return created_goal
 
     def step_zero(self):
+        """Метод проверяет какую комманду ввёл пользователь
+        и в зависимости от этого пишет пользователю ответ с дальнейшими инструкциями
+
+        """
         if self.message == '/create':
             user_categories = self.get_user_categories()
             if not user_categories:
@@ -123,6 +168,11 @@ class BotManager:
             self.tg_client.send_message(self.chat_id, f"Wrong command.\nAvailable commands:\n/goals \n/create")
 
     def step_one(self):
+        """Метод ищет категорию по номеру или названию (содержимое сообщение пользователя).
+        В случае нахождения - повышает этап пользователя до 2
+        Иначе повосторно отправляет запрос со списком категорий
+
+        """
         if category := self.get_category_by_num_or_title():
             self.tg_client.send_message(self.chat_id, f"Write goal title to create or /cancel to abort")
             self.set_user_step(2)
@@ -135,6 +185,11 @@ class BotManager:
                                         f"Also you can send /cancel to abort")
 
     def step_two(self):
+        """Метод записывает в state (состояние) title полученный отпользователя если его длина
+        не превышает 100 символов (ограничение модели БД). Иначе возвращает пояснение
+        с просит повторное введение title
+
+        """
         if len(self.message) <= 100:
             self.current_state.set_title(self.message)
             self.set_user_step(3)
@@ -145,12 +200,19 @@ class BotManager:
                                         f"Write title again")
 
     def step_three(self):
+        """Метод создает цель, обнуляет state пользователя и отправляет сообщение об успехе с данными цели
+
+        """
         created_goal = self.create_goal()
         self.current_state.clean_state()
         self.tg_client.send_message(self.chat_id, f"Goal successfully created\n"
                                         f"#{created_goal.id} {created_goal.title}")
 
     def proceed_by_steps(self):
+        """Метод проводит пользователя в зависимости от его этапа по соответствующим методам
+        Так же следит за сообщением /cancel для отмены процесса создания цели
+
+        """
         self.get_user_state()
         self.get_user_step()
 
@@ -173,13 +235,20 @@ class BotManager:
             if is_ready_to_create:
                 self.step_three()
 
-    def collect_data(self, message):
+    def collect_data(self, message: UpdateObj):
+        """Метод собирает в экземпляр необходимые для алгоритма данные
+
+        """
         self.offset = message.update_id + 1
         self.chat_id = message.message.chat.id
         self.user_id = message.message.from_.id
         self.message = message.message.text
 
     def process_response(self):
+        """Метод обрабатывает новые сообщения от пользователей, проверяет верифицирован ли пользователь,
+        верифицирует в случае необходимости. После верификации запускает основной алгоритм работы с пользователем
+
+        """
         for item in self.response.result:
             self.collect_data(item)
 
--- bot/tg/client.py
+++ bot/tg/client.py
@@ -6,22 +6,35 @@ from bot.tg.dc import GetUpdatesResponse, SendMessageResponse
 
 class TgClient:
     def __new__(cls, *args, **kwargs):
+        """Singleton pattern"""
         if not hasattr(cls, 'instance'):
             cls.instance = super().__new__(cls, *args, **kwargs)
         return cls.instance
 
     def __init__(self):
-        self.token = os.getenv('BOT_TOKEN')
-        self.link = f'https://api.telegram.org/bot{self.token}'
+        self.token: str = os.getenv('BOT_TOKEN')
+        self.link: str = f'https://api.telegram.org/bot{self.token}'
 
-    def _get_url(self, method: str):
+
+    def _get_url(self, method: str) -> str:
+        """Метод возвращает ссылку для указанного метода
+
+        """
         return f"{self.link}/{method}"
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
+        """Функция метода: получит список новых сообщений от бота
+        Метод отправляет запрос по ссылке, получает ответ, возвращает сформированный на основе ответа датакласс
+
+        """
         response = requests.get(self._get_url('getUpdates'), params={'timeout': timeout, 'offset': offset})
 
         return GetUpdatesResponse.from_dict(response.json())
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
+        """Функция метода: бот отправит сообщение пользователю
+        Метод отправляет запрос по ссылке, получает ответ, возвращает сформированный на основе ответа датакласс
+
+        """
         response = requests.get(self._get_url('sendMessage'), params={'chat_id': chat_id, 'text': text})
         return SendMessageResponse.from_dict(response.json())
